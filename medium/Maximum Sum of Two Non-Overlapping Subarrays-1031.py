d15906cb6dcc9d61df89058fda4454367b3c3fa5
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2025-01-16 19:37:53 -0500

    medium Maximum Sum of Two Non-Overlapping Subarrays 1031 in python

---
+++ medium/Maximum Sum of Two Non-Overlapping Subarrays-1031.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def maxSumTwoNoOverlap(self, nums, firstLen, secondLen):
+        """
+        :type nums: List[int]
+        :type firstLen: int
+        :type secondLen: int
+        :rtype: int
+        """
+        max_sum = 0
+        first_sum = 0
+        second_sum = 0
+        for i in range(len(nums)):
+            if i+firstLen <= len(nums):
+                first_sum = sum(nums[i:i+firstLen])
+                if i > secondLen:
+                    for ii in range(i):
+                        if ii+secondLen <= i:
+                            # print("1first: "+str(nums[i:i+firstLen])+", second: "+str(nums[ii:ii+secondLen]))
+                            second_sum = sum(nums[ii:ii+secondLen])
+                            if first_sum+second_sum>max_sum:
+                                max_sum=first_sum+second_sum
+                        
+                if (i+firstLen)+secondLen <= len(nums):
+                    for ii in range((i+firstLen), len(nums)):
+                        if ii+secondLen<=len(nums):
+                            # print("2first: "+str(nums[i:i+firstLen])+", second: "+str(nums[ii:ii+secondLen]))
+                            second_sum = sum(nums[ii:ii+secondLen])
+                            if first_sum+second_sum>max_sum:
+                                max_sum=first_sum+second_sum
+        return max_sum
+
