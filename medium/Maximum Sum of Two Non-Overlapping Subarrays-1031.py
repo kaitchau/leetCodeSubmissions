7dfb5f0a0bf6e479bb2c890394df24f68ee8c438
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2025-01-16 22:49:04 -0500

    slight mod

--- medium/Maximum Sum of Two Non-Overlapping Subarrays-1031.py
+++ medium/Maximum Sum of Two Non-Overlapping Subarrays-1031.py
@@ -21,7 +21,7 @@ class Solution(object):
                                 max_sum=first_sum+second_sum
                         
                 if (i+firstLen)+secondLen <= len(nums):
-                    for ii in range((i+firstLen), len(nums)):
+                    for ii in range((i+firstLen), len(nums)-secondLen+1):
                         if ii+secondLen<=len(nums):
                             # print("2first: "+str(nums[i:i+firstLen])+", second: "+str(nums[ii:ii+secondLen]))
                             second_sum = sum(nums[ii:ii+secondLen])
