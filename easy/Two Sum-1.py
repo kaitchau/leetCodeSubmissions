fcd28bcdef65acfb111f9fcc0a3d602e47991431
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2025-01-19 15:56:04 -0500

    added another solution to easy two sum in python

--- easy/Two Sum-1.py
+++ easy/Two Sum-1.py
@@ -1,3 +1,4 @@
+#O(N^2) time complexity
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -10,3 +11,21 @@ class Solution(object):
             for y in range(nums_len):
                 if(y>x and nums[x]+nums[y]==target):
                     return [x,y]
+
+#
+
+#O(N) time complexity?
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_len = len(nums)
+        for i in range(nums_len):
+            try:
+                j = nums[i+1:nums_len+1].index(target-nums[i])
+                return [i,j+i+1]
+            except:
+                continue
