ff37693ffbc436844475b6aea4c95e3c3c1d5541
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2025-01-19 19:28:01 -0500

    added more efficient two sum solution in python

--- easy/Two Sum-1.py
+++ easy/Two Sum-1.py
@@ -29,3 +29,15 @@ class Solution(object):
                 return [i,j+i+1]
             except:
                 continue
+
+#
+
+#O(N) time complexity
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prev = {}
+        for i,n in enumerate(nums):
+            diff = target-n
+            if diff in prev:
+                return [i,prev[diff]]
+            prev[n]=i
