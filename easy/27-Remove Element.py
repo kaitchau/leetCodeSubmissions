24a9b245b46a62db12b670f32d445932340eb2a0
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2025-01-27 18:38:15 -0500

    easy 27 remove element in python

--- easy/27-Remove Element.py
+++ easy/27-Remove Element.py
@@ -3,3 +3,14 @@ class Solution:
         while val in nums:
             nums.remove(val)
         return len(nums)
+
+#
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        index=0
+        for n in nums:
+            if n!=val:
+                nums[index]=n
+                index+=1
+        return index
