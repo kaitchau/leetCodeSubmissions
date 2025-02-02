7469ca25c01a00738de71e88909d5b4b39876466
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2025-02-02 14:14:02 -0500

    added slightly different solution

--- medium/347-Top K Frequent Elements.py
+++ medium/347-Top K Frequent Elements.py
@@ -15,3 +15,24 @@ class Solution:
         for key in sorted_freq:
             ans.extend(sorted_freq[key])
         return ans[-k:]
+
+#
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        ans = []
+        numfreq={}
+        for n in nums:
+            if n in numfreq:
+                numfreq[n]+=1
+            else:
+                numfreq[n]=1
+        freqnum = defaultdict(list)
+        for num, freq in numfreq.items():
+            freqnum[freq].append(num)        
+        sorted_dict = dict(sorted(freqnum.items()))
+        for key in sorted_dict:
+            for item in sorted_dict[key]:
+                ans.append(item)
+        return ans[-k:]
+        
