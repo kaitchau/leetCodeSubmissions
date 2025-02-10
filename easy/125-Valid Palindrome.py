ab60cd42dca594623c00ffd50c1742721eb7e93d
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2025-02-10 15:14:44 -0500

    easy 125 valid palindrome two pters solution in python

--- easy/125-Valid Palindrome.py
+++ easy/125-Valid Palindrome.py
@@ -19,3 +19,22 @@ class Solution:
             if c.isalnum():
                 palindrome+=c.lower()
         return palindrome == palindrome[::-1]
+
+#
+
+#O(n)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left,right=0,len(s)-1
+        while left<right:
+            if s[left].isalnum() and s[right].isalnum():
+                if s[left].lower()==s[right].lower():
+                    left+=1
+                    right-=1
+                else:
+                    return False
+            elif not s[left].isalnum():
+                left+=1
+            elif not s[right].isalnum():
+                right-=1
+        return True
