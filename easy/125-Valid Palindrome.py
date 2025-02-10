a9688243aa679eccbff3bf41c9220b2d953ce4f0
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2025-02-09 22:40:32 -0500

    easy 125 valid palindrome in python

--- easy/125-Valid Palindrome.py
+++ easy/125-Valid Palindrome.py
@@ -1,3 +1,4 @@
+#O(n)
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         palindrome = ''
@@ -6,4 +7,15 @@ class Solution:
                 palindrome+= c.lower()
             elif c.isdigit():
                 palindrome+=c
+        return palindrome == palindrome[::-1]
+
+#
+
+#O(n)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        palindrome = ''
+        for c in s:
+            if c.isalnum():
+                palindrome+=c.lower()
         return palindrome == palindrome[::-1]
