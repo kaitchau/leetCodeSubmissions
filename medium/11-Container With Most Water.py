7f3c343e6a7d239a5a86ecb98f5fe45292b02a4c
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2025-03-08 15:04:17 -0500

    medium 11 container with most water in python two pters

--- medium/11-Container With Most Water.py
+++ medium/11-Container With Most Water.py
@@ -8,3 +8,18 @@ class Solution:
                 area = min(height[i],height[j])*(j-i)
                 max_area=max(max_area,area)
         return max_area
+
+# two pointer O(n) time complexity O(1) space complexity
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n=len(height)
+        l,r = 0,n-1
+        res=0
+        while l<r:
+            area = min(height[l],height[r])*(r-l)
+            res=max(res,area)
+            if height[l]<height[r]:
+                l+=1
+            else:
+                r-=1
+        return res
