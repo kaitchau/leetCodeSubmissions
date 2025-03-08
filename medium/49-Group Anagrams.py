c71c133eff4f6e8440016aa59dc259e4179ae8d8
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2025-03-08 14:55:05 -0500

    redid group anagrams

--- medium/49-Group Anagrams.py
+++ medium/49-Group Anagrams.py
@@ -11,3 +11,24 @@ class Solution:
         for group in groups:
             ans.append(groups[group])
         return ans
+
+#
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups={}
+        ans=[]
+        for string in strs:
+            s=''.join(sorted(string))
+            groups[s].append(string)
+        return list(groups.values())
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
+        for s in strs:
+            ss=''.join(sorted(s))
+            hashmap[ss].append(s)
+        return list(hashmap.values())
+
+
